efbf2f1b0037bbb55ba2581d38efe75eeb8980f7
0n40 <90311300+0n40@users.noreply.github.com>
2021-09-08 18:35:55 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo /etc/init.d/nginx stop
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx start
+cat /etc/nginx/nginx.conf
+sudo rm -rf /etc/nginx/site-enable/default
+sudo /etc/init.d/nginx restart
