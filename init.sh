bb3e4898710851edcead303e142ed4a37d00f949
root <root@astra>
2021-09-09 14:09:08 +0600

    переименовано: etc/hello.py -> hello.py 	изменено:      init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ cat /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/site-enable/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-cd /home/box/web/etc && gunicorn -b 0.0.0.0:8080 hello:app
+cd /home/box/web && gunicorn -b 0.0.0.0:8080 hello:app
