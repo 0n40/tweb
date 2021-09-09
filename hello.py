6d4fb6ecfdffba58de5595cc361caddd9c032e4f
root <root@astra>
2021-09-09 14:11:52 +0600

    изменено:      hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def app(param, start_response):
     """Simplest possible application object"""
+    print(param['QUERY_STRING'])
     data=[x+'\n' for x in param['QUERY_STRING'].split('&')]
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
